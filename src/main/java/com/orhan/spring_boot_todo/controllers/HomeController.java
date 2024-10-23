0ff92f387b0814d18eaf1275986aad075457b81d
Orhan <orhanbayram001@gmail.com>
2024-10-23 15:31:09 +0300

    Update HomeController.java

--- src/main/java/com/orhan/spring_boot_todo/controllers/HomeController.java
+++ src/main/java/com/orhan/spring_boot_todo/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package com.orhan.spring_boot_todo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.orhan.spring_boot_todo.services.TodoItemService;
 
-@Controller
+@RestController
 public class HomeController {
 
 	
